0bd8c5fb9b6ba640771673c07cf6dbb2275d242f
brunosousavidal <77804171+brunosousavidal@users.noreply.github.com>
2021-03-05 16:57:18 -0300

    Update tempo.js
    Correção do codigo

--- pages/tempo.js
+++ pages/tempo.js
@@ -6,6 +6,7 @@ function Tempo(props) {
   return (
     <div>
       <div>{dynamicDateString} (dinâmico)</div>
+      {/* <div>{props.staticDateString} (estático com delay)</div> */}
       <div>{props.staticDateString} (estático)</div>
     </div>
 	);
@@ -13,15 +14,18 @@ function Tempo(props) {
 
 export async function getStaticProps() {
   console.log("> Passando pelo getStaticProps();");
+//  console.log("> Adicionando delay de 5 segundos;");
 
+//  await delay(5000);
   const staticDate = new Date();
   const staticDateString = staticDate.toGMTString();
   return {
     props: {
       staticDateString
     },
-    revalidate: 1 //revalida a parte estatica a cada segundo
+    revalidate: 1 //revalida a parte estatica a cada segundo    
   }
 }
-
+//simulando um delay bem ruim na hora do build
+//const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 export default Tempo;
