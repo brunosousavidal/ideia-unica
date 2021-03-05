649c1391cf8ef99e90ebd578f71b3f0f791fb5a4
brunosousavidal <77804171+brunosousavidal@users.noreply.github.com>
2021-03-05 18:13:38 -0300

    incluir variavel de ambiente
    variavel de ambiente com a chave secreta de acesso ao convertkit

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,10 @@
-function tempo(request, response) {
+async function tempo(request, response) {
+  const apiSecret = process.env.CONVERTKIT_API_SECRET;
   const dynamicDate = new Date();
 	
   //API de acesso ao newsletter do felipe para pegar a quantidade de acessos
-  const subscriberResponse = await fetch("https://api.convertkit.com/v3/subscribers?api_secret=ZJwLOhi01EvB90Clpsyw1e0fGRzPfw1Q");
+  //https://api.convertkit.com/v3/subscribers?api_secret=ZJwLOhi01EvB90Clpsyw1e0fGRzPfw1Q
+  const subscriberResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
   //recebe no formato de um json
   const subscriberResponseJson =await subscriberResponse.json();
   //pega somente o total de inscritos
