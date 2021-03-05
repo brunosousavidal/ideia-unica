55e5880633cd3755b2c5fc31fdd42a3632cfd649
brunosousavidal <77804171+brunosousavidal@users.noreply.github.com>
2021-03-05 17:34:32 -0300

    incluido a consulta ao convertkit
    acesso a api do convertkit para saber a quantidade de inscritos na newsletter no felipe

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,16 @@
 function tempo(request, response) {
   const dynamicDate = new Date();
 	
+  //API de acesso ao newsletter do felipe para pegar a quantidade de acessos
+  const subscriberResponse = await fetch("https://api.convertkit.com/v3/subscribers?api_secret=ZJwLOhi01EvB90Clpsyw1e0fGRzPfw1Q");
+  //recebe no formato de um json
+  const subscriberResponseJson =await subscriberResponse.json();
+  //pega somente o total de inscritos
+  const inscritos = subscriberResponseJson.total_subscribers;
+  
   response.json({
-    date: dynamicDate.toGMTString()
+    date: dynamicDate.toGMTString(),
+    inscritos: inscritos
   }) 
 }
 
